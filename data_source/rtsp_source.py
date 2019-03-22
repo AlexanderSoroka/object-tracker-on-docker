f45556b33b9a8574b80204ce9237bcec1a15be26
Alexander Soroka <soroka.a.m@gmail.com>
2019-03-22 19:15:37 +0300

    Dynamically skip frames if processing FPS is lower than source FPS

--- data_source/rtsp_source.py
+++ data_source/rtsp_source.py
@@ -1,13 +1,23 @@
 """
-RSTP data source implements python iterator interface. Grab one frame on demand without caching.
+RTSP data source implements python iterator interface. Grab one frame on demand without caching.
 Can freeze caller if request rate is higher then stream framerate
 """
 
-
 __author__ = "Alexander Soroka, soroka.a.m@gmail.com"
 
-
 import cv2
+import time
+
+from multiprocessing import Queue, Process
+
+
+def stream_reader(uri, queue):
+    stream = cv2.VideoCapture(uri)
+    while stream.grab():
+        if queue.empty():
+            succeeded, frame = stream.retrieve()
+            queue.put(frame)
+#        time.sleep(0.01)
 
 
 class RTSPSource:
@@ -19,7 +29,9 @@ class RTSPSource:
         Initialize new stream with specified uri
         :param uri: RSTP source uri
         """
-        self.__stream = cv2.VideoCapture(uri)
+        self.__frame_queue = Queue()
+        self.__reader = Process(target=stream_reader, args=(uri, self.__frame_queue))
+        self.__reader.start()
 
     def __iter__(self):
         return self
@@ -28,8 +40,9 @@ class RTSPSource:
         return self.next()
 
     def next(self):
-        succeeded, frame = self.__stream.read()
-        if succeeded:
-            return frame
-        else:
-            raise StopIteration()
+        try:
+            frame = self.__frame_queue.get(block=False)
+        except:
+            raise StopIteration
+
+        return frame
